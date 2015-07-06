1062b7bb8c2734cc7a0db45f968e885e44f24b93
alfred sang <i5ting@126.com>
2015-07-06 08:28:02 +0800

    init

--- index.js
+++ index.js
@@ -16,18 +16,18 @@ if ( argv.length > 1 ) {
 }
 
 // execFile: executes a file with the specified arguments
-child_process.exec('cat *.js bad_file | wc -l',
-  function (error, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-    if (error !== null) {
-      console.log('exec error: ' + error);
-    }
-});
-
-// execFile: executes a file with the specified arguments
-// child_process.execFile(file_path + '/bson.sh',null,{cwd:current_path},function (error,stdout,stderr) {
+// child_process.exec('cat *.js bad_file | wc -l',
+//   function (error, stdout, stderr) {
+//     console.log('stdout: ' + stdout);
+//     console.log('stderr: ' + stderr);
 //     if (error !== null) {
 //       console.log('exec error: ' + error);
 //     }
 // });
+
+// execFile: executes a file with the specified arguments
+child_process.execFile(file_path + '/do.sh',null,{cwd:current_path},function (error,stdout,stderr) {
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+});
