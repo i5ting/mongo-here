618d08e49ab9cfe0dc3a44c7f5c35004af09a99a
alfred sang <i5ting@126.com>
2015-08-03 15:53:25 +0800

    init mhg

---
+++ global.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+var child_process = require('child_process');
+
+var argv = process.argv;
+argv.shift();
+
+var file_path = __dirname;
+var current_path = process.cwd();
+
+if ( argv.length > 1 ) {
+  var first_arg = argv[0];
+  if ( first_arg == '-h' || first_arg == '--help' ) {
+    console.log('mh')
+  }
+}
+
+require('shelljs/global');
+
+if (!which('mongod')) {
+  echo('Sorry, this script requires mongod');
+  exit(1);
+}
+
+
+// execFile: executes a file with the specified arguments
+// child_process.exec('cat *.js bad_file | wc -l',
+//   function (error, stdout, stderr) {
+//     console.log('stdout: ' + stdout);
+//     console.log('stderr: ' + stderr);
+//     if (error !== null) {
+//       console.log('exec error: ' + error);
+//     }
+// });
+
+// execFile: executes a file with the specified arguments
+child_process.execFile(file_path + '/global.sh',null,{cwd:current_path},function (error,stdout,stderr) {
+    if (error !== null) {
+      console.log('start mongo here global exec error: ' + error);
+    }else{
+      console.log('start mongo here global success!')
+    }
+});
---
+++ global.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+mkdir -p ~/mongo/db
+mkdir -p ~/mongo/pids
+mkdir -p ~/mongo/logs
+
+touch ~/mongo/pids/mongodb.pid
+
+# mongod --bind_ip 192.168.1.100 --port 27017 --dbpath tmp/db --logpath tmp/logs/mongodb.log --pidfilepath tmp/pids/mongodb.pid
+nohup mongod --bind_ip 127.0.0.1 --port 27017 --dbpath ~/mongo/db --logpath ~/mongo/logs/mongodb.log --pidfilepath ~/mongo/pids/mongodb.pid > ~/mongo/logs/mongod.log 2>&1 &
