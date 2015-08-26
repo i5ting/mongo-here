110ab39261ad67548698b6bec56867741e29452e
i5ting <i5ting@126.com>
2015-08-26 10:02:26 +0800

    add remove lock file if lock file exist

--- do.sh
+++ do.sh
@@ -4,6 +4,10 @@ mkdir -p tmp/db
 mkdir -p tmp/pids
 mkdir -p tmp/logs
 
+
+# remove lock file
+[ -f tmp/db/mongod.lock ] && rm -rf tmp/db/mongod.lock
+
 touch tmp/pids/mongodb.pid
 
 # mongod --bind_ip 192.168.1.100 --port 27017 --dbpath tmp/db --logpath tmp/logs/mongodb.log --pidfilepath tmp/pids/mongodb.pid
